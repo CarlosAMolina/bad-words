aa2ca2e2489747300343e5588c02c4065d8a7edd
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-05-28 22:56:33 +0200

    Add improve error message

--- rust/bad-words/src/main.rs
+++ rust/bad-words/src/main.rs
@@ -2,7 +2,8 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = env::var("BAD_WORDS_API_KEY").expect("No environment variable");
+    const ENV_VARIABLE: &str = "BAD_WORDS_API_KEY";
+    let api_key = env::var(ENV_VARIABLE).expect(&format!("env variable {} not set", ENV_VARIABLE));
     let client = reqwest::Client::new();
     let res = client
         .post("https://api.apilayer.com/bad_words?censor_character=*")
